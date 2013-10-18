d44b28f7e4df2ffe74cec198bac07267b2190bd1
Grzegorz Bunia <xenon2185@gmail.com>
2013-10-18 14:12:00 +0200

    Voting enhancement: User can vote only once on particular comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -34,19 +34,27 @@ class CommentsController < ApplicationController
 
   def vote_up
     comment = Comment.find(params[:comment_id])
-    vote = comment.votes.build(type: :up)
-    vote.user = current_user
-    vote.save
-    redirect_to post_path(comment.post), notice: "You've just voted up comment '#{comment.title}'"
+    if already_voted?(comment)
+      redirect_to post_path(comment.post), alert: "You've already voted on comment '#{comment.title}'"
+    else
+      vote = comment.votes.build(type: :up)
+      vote.user = current_user
+      vote.save
+      redirect_to post_path(comment.post), notice: "You've just voted up comment '#{comment.title}'"
+    end
   end
 
   def vote_down
     comment = Comment.find(params[:comment_id])
-    vote = comment.votes.build(type: :down)
-    vote.user = current_user
-    vote.save
-    comment.update_attributes(abusive: true) if comment.votes.where(type: :down).count == 3
-    redirect_to post_path(comment.post), notice: "You've just voted down comment '#{comment.title}'"
+    if already_voted?(comment)
+      redirect_to post_path(comment.post), alert: "You've already voted on comment '#{comment.title}'"
+    else
+      vote = comment.votes.build(type: :down)
+      vote.user = current_user
+      vote.save
+      comment.update_attributes(abusive: true) if comment.votes.where(type: :down).count == 3
+      redirect_to post_path(comment.post), notice: "You've just voted down comment '#{comment.title}'"
+    end
   end
 
   def mark_not_abusive
@@ -55,5 +63,10 @@ class CommentsController < ApplicationController
     comment.save
     redirect_to post_path(comment.post), notice: "Comment '#{comment.title}' marked as not abusive."
   end
+
+  private
+    def already_voted? comment
+      ! comment.votes.where(user: current_user).empty?
+    end
 
 end
